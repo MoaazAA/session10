b9d0aa53fcc659d46edff6c763cdba6db884c52b
MoaazAA <moaazabdelhamid1@gmail.com>
2021-08-17 17:45:13 +0200

    session10

---
+++ js/index2.js
@@ -0,0 +1,18 @@
+var y = 5
+var x = 10
+var area = y*x
+var perimeter = y+x
+
+console.log(y)
+console.log(x)
+console.log(area)
+console.log(perimeter)
+
+
+var choice = 3000
+
+if(choice <= 3000){
+    console.log('you should to pay bigger than 3000')
+}else{
+    console.log('it is okay')
+}
